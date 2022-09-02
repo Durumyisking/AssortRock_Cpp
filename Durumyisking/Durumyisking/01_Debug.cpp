61eef0c319392a45e07afab715b37c44ad0c4830
Durumyisking <80432748+Durumyisking@users.noreply.github.com>
2022-09-02 19:42:20 +0900

    20220902 Commit
    01_Debug 학습
    
    수업때 사용할
    Durumyisking 프로젝트 생성

---
+++ Durumyisking/Durumyisking/01_Debug.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int main()
+{
+
+	// 텍스트 출력
+	std::cout << "Hello World!";
+	std::cout << "Hello World!";
+	std::cout << "Hello World!";
+	// std::cout << "Hello World!";
+
+	/*
+	이것은 주석입니다.
+	*/
+
+	// 이 사이에는 중단점이 들어가요
+	int i = 0;
+	// 이 사이에는 중단점이 들어가요
+
+
+	// 프로그램 종료
+	return 0;
+
+}
